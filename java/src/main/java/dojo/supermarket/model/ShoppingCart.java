d8bde2ff02b139494afe2f358ce4f07aef736066
Emily Bache <emily.bache@praqma.com>
2019-05-09 21:14:19 +0200

    "r extract method"

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -42,40 +42,44 @@ public class ShoppingCart {
     private void handleProduct(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog, Product p) {
         double quantity = productQuantities.get(p);
         if (offers.containsKey(p)) {
-            Offer offer = offers.get(p);
-            double unitPrice = catalog.getUnitPrice(p);
-            int quantityAsInt = (int) quantity;
-            Discount discount = null;
-            int x = 1;
-            if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                x = 3;
-
-            } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                x = 2;
-                if (quantityAsInt >= 2) {
-                    double total = offer.argument * quantityAsInt / x + quantityAsInt % 2 * unitPrice;
-                    double discountN = unitPrice * quantity - total;
-                    discount = new Discount(p, "2 for " + offer.argument, discountN);
-                }
-
-            }
-            if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                x = 5;
-            }
-            int numberOfXs = quantityAsInt / x;
-            if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
-                discount = new Discount(p, "3 for 2", discountAmount);
-            }
-            if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-                discount = new Discount(p, offer.argument + "% off", quantity * unitPrice * offer.argument / 100.0);
-            }
-            if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                discount = new Discount(p, x + " for " + offer.argument, discountTotal);
-            }
-            if (discount != null)
-                receipt.addDiscount(discount);
+            Bang(receipt, offers, catalog, p, quantity);
         }
+    }
+
+    private void Bang(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog, Product p, double quantity) {
+        Offer offer = offers.get(p);
+        double unitPrice = catalog.getUnitPrice(p);
+        int quantityAsInt = (int) quantity;
+        Discount discount = null;
+        int x = 1;
+        if (offer.offerType == SpecialOfferType.ThreeForTwo) {
+            x = 3;
+
+        } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
+            x = 2;
+            if (quantityAsInt >= 2) {
+                double total = offer.argument * quantityAsInt / x + quantityAsInt % 2 * unitPrice;
+                double discountN = unitPrice * quantity - total;
+                discount = new Discount(p, "2 for " + offer.argument, discountN);
+            }
+
+        }
+        if (offer.offerType == SpecialOfferType.FiveForAmount) {
+            x = 5;
+        }
+        int numberOfXs = quantityAsInt / x;
+        if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+            double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+            discount = new Discount(p, "3 for 2", discountAmount);
+        }
+        if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
+            discount = new Discount(p, offer.argument + "% off", quantity * unitPrice * offer.argument / 100.0);
+        }
+        if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+            double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+            discount = new Discount(p, x + " for " + offer.argument, discountTotal);
+        }
+        if (discount != null)
+            receipt.addDiscount(discount);
     }
 }
