c4d17d9b9e5eb4bf86e402b0c99aa2aae01dc78e
Vinay S Shenoy <vinay@obvious.in>
2019-07-20 22:21:07 +0530

    Add characteristics tests for calculating total price w/o discount

--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -1,27 +1,80 @@
 package dojo.supermarket.model
 
 import org.junit.jupiter.api.Test
-import supermarket.model.*
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import supermarket.model.Product
+import supermarket.model.ProductUnit
+import supermarket.model.ShoppingCart
+import supermarket.model.Teller
 
 class SupermarketTest {
 
+    private val catalog = FakeCatalog()
+    private val cart = ShoppingCart()
+    private val teller = Teller(catalog)
+
     @Test
-    fun testSomething() {
-        val catalog = FakeCatalog()
-        val toothbrush = Product("toothbrush", ProductUnit.Each)
-        catalog.addProduct(toothbrush, 0.99)
-        val apples = Product("apples", ProductUnit.Kilo)
-        catalog.addProduct(apples, 1.99)
+    fun `total price for product unit of type Each must be calculated`() {
+        // given
+        val product1 = Product("product 1", ProductUnit.Each)
+        val product2 = Product("product 2", ProductUnit.Each)
 
-        val cart =
-            ShoppingCart()
-        cart.addItemQuantity(apples, 2.5)
+        catalog.apply {
+            addProduct(product = product1, price = 100.0)
+            addProduct(product = product2, price = 50.0)
+        }
 
-        val teller = Teller(catalog)
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0)
-
+        // when
+        cart.apply {
+            addItemQuantity(product = product1, quantity = 2.0)
+            addItemQuantity(product = product2, quantity = 3.0)
+        }
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // Todo: complete this test
+        // then
+        expectThat(receipt.totalPrice).isEqualTo(350.0)
+    }
+
+    @Test
+    fun `total price for product unit of type Kilo must be calculated`() {
+        val product1 = Product("product 1", ProductUnit.Kilo)
+        val product2 = Product("product 2", ProductUnit.Kilo)
+
+        catalog.apply {
+            addProduct(product = product1, price = 5.0)
+            addProduct(product = product2, price = 0.75)
+        }
+
+        // when
+        cart.apply {
+            addItemQuantity(product = product1, quantity = 2.0)
+            addItemQuantity(product = product2, quantity = 3.0)
+        }
+        val receipt = teller.checksOutArticlesFrom(cart)
+
+        // then
+        expectThat(receipt.totalPrice).isEqualTo(12.25)
+    }
+
+    @Test
+    fun `total price for product unit of all types must be calculated`() {
+        val product1 = Product("product 1", ProductUnit.Kilo)
+        val product2 = Product("product 2", ProductUnit.Each)
+
+        catalog.apply {
+            addProduct(product = product1, price = 10.0)
+            addProduct(product = product2, price = 0.5)
+        }
+
+        // when
+        cart.apply {
+            addItemQuantity(product = product1, quantity = 0.5)
+            addItemQuantity(product = product2, quantity = 3.0)
+        }
+        val receipt = teller.checksOutArticlesFrom(cart)
+
+        // then
+        expectThat(receipt.totalPrice).isEqualTo(6.5)
     }
 }
