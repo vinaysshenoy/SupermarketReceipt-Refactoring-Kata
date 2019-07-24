3643dd09e66df1ef0e0f6d776d541326ff95bdfb
Vinay S Shenoy <vinay@obvious.in>
2019-07-24 21:39:46 +0530

    Make Offer constructor private
    Creation of the Offer object is now delegated to a static factory on the
    Offer class.

--- kotlin/src/main/kotlin/supermarket/model/Offer.kt
+++ kotlin/src/main/kotlin/supermarket/model/Offer.kt
@@ -1,3 +1,10 @@
 package supermarket.model
 
-class Offer(internal var offerType: SpecialOfferType, internal val product: Product, internal var argument: Double)
+class Offer private constructor(val offerType: SpecialOfferType, val product: Product, val argument: Double) {
+
+    companion object {
+        fun create(offerType: SpecialOfferType, product: Product, argument: Double): Offer {
+            return Offer(offerType, product, argument)
+        }
+    }
+}
