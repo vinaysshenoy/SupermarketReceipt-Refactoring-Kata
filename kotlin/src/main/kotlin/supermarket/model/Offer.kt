1d08dc8e21ee0b45723dba3977c0adaa72c4adb1
Vinay S Shenoy <vinay@obvious.in>
2019-07-24 21:58:50 +0530

    Change Offer to a sealed class, create different offer types

--- kotlin/src/main/kotlin/supermarket/model/Offer.kt
+++ kotlin/src/main/kotlin/supermarket/model/Offer.kt
@@ -1,10 +1,59 @@
 package supermarket.model
 
-class Offer private constructor(val offerType: SpecialOfferType, val product: Product, val argument: Double) {
+sealed class Offer {
+
+    abstract val offerType: SpecialOfferType
+
+    abstract val product: Product
+
+    abstract val argument: Double
+
+    data class ThreeForTwo(
+        override val product: Product
+    ) : Offer() {
+
+        override val offerType = SpecialOfferType.ThreeForTwo
+
+        override val argument = -1.0
+    }
+
+    data class TenPercentDiscount(
+        override val product: Product
+    ) : Offer() {
+
+        override val offerType = SpecialOfferType.TenPercentDiscount
+
+        override val argument = 10.0
+    }
+
+    data class TwoForAmount(
+        override val product: Product,
+        val amount: Double
+    ) : Offer() {
+
+        override val offerType = SpecialOfferType.TwoForAmount
+
+        override val argument = amount
+    }
+
+    data class FiveForAmount(
+        override val product: Product,
+        val amount: Double
+    ) : Offer() {
+
+        override val offerType = SpecialOfferType.FiveForAmount
+
+        override val argument = amount
+    }
 
     companion object {
         fun create(offerType: SpecialOfferType, product: Product, argument: Double): Offer {
-            return Offer(offerType, product, argument)
+            return when (offerType) {
+                SpecialOfferType.ThreeForTwo -> ThreeForTwo(product = product)
+                SpecialOfferType.TenPercentDiscount -> TenPercentDiscount(product = product)
+                SpecialOfferType.TwoForAmount -> TwoForAmount(product = product, amount = argument)
+                SpecialOfferType.FiveForAmount -> FiveForAmount(product = product, amount = argument)
+            }
         }
     }
 }
