561b114197c725f8b4046e92fb729c1298ac99f9
Vinay S Shenoy <vinay@obvious.in>
2019-07-24 22:29:24 +0530

    Move finding of applicable discount for an offer to separate method

--- kotlin/src/main/kotlin/supermarket/model/ShoppingCart.kt
+++ kotlin/src/main/kotlin/supermarket/model/ShoppingCart.kt
@@ -36,39 +36,52 @@ class ShoppingCart {
             if (offers.containsKey(p)) {
                 val offer = offers[p]!!
                 val unitPrice = catalog.getUnitPrice(p)
-                val quantityAsInt = quantity.toInt()
-                var discount: Discount? = null
-                if (offer.offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    val minimumQuantityToApplyOffer = 3
-                    val numberOfXs = quantityAsInt / minimumQuantityToApplyOffer
-                    val discountAmount =
-                        quantity * unitPrice - (numberOfXs.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
-                    discount = Discount(p, "3 for 2", discountAmount)
 
-                } else if (offer.offerType === SpecialOfferType.TwoForAmount) {
-                    val minimumQuantityToApplyOffer = 2
-                    if (quantityAsInt >= 2) {
-                        val total =
-                            offer.argument * quantityAsInt / minimumQuantityToApplyOffer + quantityAsInt % 2 * unitPrice
-                        val discountN = unitPrice * quantity - total
-                        discount = Discount(p, "2 for " + offer.argument, discountN)
-                    }
-
-                } else if (offer.offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    val minimumQuantityToApplyOffer = 5
-                    val numberOfXs = quantityAsInt / minimumQuantityToApplyOffer
-                    val discountTotal =
-                        unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice)
-                    discount =
-                        Discount(p, minimumQuantityToApplyOffer.toString() + " for " + offer.argument, discountTotal)
-                } else if (offer.offerType === SpecialOfferType.TenPercentDiscount) {
-                    discount =
-                        Discount(p, offer.argument.toString() + "% off", quantity * unitPrice * offer.argument / 100.0)
-                }
-                if (discount != null)
+                discountIfOfferApplicable(
+                    offer = offer,
+                    quantity = quantity,
+                    unitPrice = unitPrice,
+                    product = p
+                )?.let { discount ->
                     receipt.addDiscount(discount)
+                }
             }
-
         }
+    }
+
+    private fun discountIfOfferApplicable(
+        offer: Offer,
+        quantity: Double,
+        unitPrice: Double,
+        product: Product
+    ): Discount? {
+
+        val quantityAsInt = quantity.toInt()
+
+        return if (offer.offerType === SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+            val minimumQuantityToApplyOffer = 3
+            val numberOfXs = quantityAsInt / minimumQuantityToApplyOffer
+            val discountAmount =
+                quantity * unitPrice - (numberOfXs.toDouble() * 2.0 * unitPrice + quantityAsInt % 3 * unitPrice)
+            Discount(product, "3 for 2", discountAmount)
+
+        } else if (offer.offerType === SpecialOfferType.TwoForAmount) {
+            val minimumQuantityToApplyOffer = 2
+            if (quantityAsInt >= 2) {
+                val total =
+                    offer.argument * quantityAsInt / minimumQuantityToApplyOffer + quantityAsInt % 2 * unitPrice
+                val discountN = unitPrice * quantity - total
+                Discount(product, "2 for " + offer.argument, discountN)
+            } else null
+
+        } else if (offer.offerType === SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+            val minimumQuantityToApplyOffer = 5
+            val numberOfXs = quantityAsInt / minimumQuantityToApplyOffer
+            val discountTotal =
+                unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice)
+            Discount(product, minimumQuantityToApplyOffer.toString() + " for " + offer.argument, discountTotal)
+        } else if (offer.offerType === SpecialOfferType.TenPercentDiscount) {
+            Discount(product, offer.argument.toString() + "% off", quantity * unitPrice * offer.argument / 100.0)
+        } else null
     }
 }
